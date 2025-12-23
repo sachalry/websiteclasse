d9bb14eb22b440d65262c8fbedf634c67627580b
Sacha LEROY <sachaler25@gmail.com>
2025-12-23 10:26:58 +0100

    v3.2

--- static/script.js
+++ static/script.js
@@ -1,1 +1,21 @@
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const links = document.querySelectorAll('header__link');
 
+links.forEach(link => {
+    link.classList.add('hide')
+});
+
+main.classList.add('hide')
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    // wait a second before adding the class
+    setTimeout(() => {
+        header.classList.add('loaded')
+        main.classList.remove('hide')
+        links.forEach(link => {
+            link.classList.remove('hide')
+        });
+    }, 500);
+});
